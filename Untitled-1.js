c4906ee9cc44935bb4b3cc7bfe3dac708e8b0a15
babu169 <151108315+babu169@users.noreply.github.com>
2023-11-17 08:38:25 +0530

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,17 @@
+<script>
+function biggest_of_three_numbers(variable1, variable2, variable3){
+    if(variable1 > variable2){
+        if(variable1 > variable3){
+            console.log("The biggest of three is: ", variable1);
+        }else{
+            console.log("The biggest of three is: ", variable3);
+        }
+    }else{
+        if(variable2 > variable3){
+            console.log("The biggest of three is: ", variabl2);
+        }else{
+            console.log("The biggest of three is: ", variable3);
+        }
+    }
+}
+<script>
